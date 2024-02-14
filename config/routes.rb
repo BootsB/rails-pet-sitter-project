78533f3ebe60270de7259a1471da0a3d92986644
boots britton <boots.britton@yahoo.co.uk>
2024-02-14 19:32:39 +0000

    changes to requests controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users do
+    resources :requests
+  end
+
+  resources :pets do
+    resources :requests
+  end
+
   root to: "pages#home"
-  resources :pets
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_02_14_184722) do
+ActiveRecord::Schema[7.0].define(version: 2024_02_14_191029) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -34,6 +34,7 @@ ActiveRecord::Schema[7.0].define(version: 2024_02_14_184722) do
     t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "accepted", default: false
     t.index ["pet_id"], name: "index_requests_on_pet_id"
     t.index ["user_id"], name: "index_requests_on_user_id"
   end
