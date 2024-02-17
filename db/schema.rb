90a913009cf65b4ae9b79c0c9badd555062bba47
Cristian Manolache <mlcristian@outlook.com>
2024-02-17 21:37:36 +0000

    added the missing age in the profile

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_02_17_133117) do
+ActiveRecord::Schema[7.0].define(version: 2024_02_17_213542) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -36,6 +36,7 @@ ActiveRecord::Schema[7.0].define(version: 2024_02_17_133117) do
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "age"
     t.index ["user_id"], name: "index_profiles_on_user_id"
   end
 
