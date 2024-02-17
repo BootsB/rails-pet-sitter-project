8db1ce584e420c1faf9c1f764c0ae4abdd4f22f0
boots britton <boots.britton@yahoo.co.uk>
2024-02-17 14:57:38 +0000

    pet#show

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -1,5 +1,12 @@
 class PetsController < ApplicationController
+  before_action :set_pet, only: %i[show]
   def index
     @pets = Pet.all
+  end
+  def show
+  end
+
+  def set_pet
+    @pet = Pet.find(params[:id])
   end
 end
