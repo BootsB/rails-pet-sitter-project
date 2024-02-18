53349e1a981c3a5601ec4c3cac1ed4f11344fc34
Cristian Manolache <mlcristian@outlook.com>
2024-02-18 12:04:34 +0000

    Removed Columns In User

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_02_17_213542) do
+ActiveRecord::Schema[7.0].define(version: 2024_02_18_120356) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -51,10 +51,6 @@ ActiveRecord::Schema[7.0].define(version: 2024_02_17_213542) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "first_name"
-    t.string "last_name"
-    t.string "phone_number"
-    t.text "description"
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
