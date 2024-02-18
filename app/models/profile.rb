c9050748c424c9a104a5318502e7763168615da5
Cristian Manolache <mlcristian@outlook.com>
2024-02-18 17:47:25 +0000

    fix image upload on create view

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -2,4 +2,6 @@ class Profile < ApplicationRecord
   belongs_to :user
   validates :user_id, presence: true
   has_one_attached :photo
+
+  enum role: { pet_owner: "pet_owner", pet_sitter: "pet_sitter" }
 end
