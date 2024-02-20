66ca79ff1c9b13f343b7493b031d3803f10e062e
Cristian Manolache <mlcristian@outlook.com>
2024-02-20 19:29:42 +0000

    added the listing for the pets with logic cases in the navbar

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,16 @@ class User < ApplicationRecord
   has_many :pets
   has_many :requests
   has_one  :profile
+
+  def pet_owner?
+    profile.present? && profile.role == 'pet_owner'
+  end
+
+  def pet_sitter?
+    profile.present? && profile.role == 'pet_sitter'
+  end
+
+  def both?
+    profile.present? && profile.role == 'both'
+  end
 end
