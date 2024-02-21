737888850a39870567fb4717286356ce0c195be7
boots britton <boots.britton@yahoo.co.uk>
2024-02-21 18:03:52 +0000

    small changes

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -25,7 +25,7 @@ class PetsController < ApplicationController
     @pet = Pet.new(pet_params)
     @pet.user = current_user
     if @pet.save
-      redirect_to pets_path(@pet)
+      redirect_to pet_path(@pet)
     else
       render :new, status: :unprocessable_entity
     end
