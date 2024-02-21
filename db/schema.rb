31ed1921c780d51750b25b2c38d3cd9aae2936d6
Cristian Manolache <mlcristian@outlook.com>
2024-02-21 14:09:45 +0000

    founded this not commited

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_02_19_095725) do
+ActiveRecord::Schema[7.0].define(version: 2024_02_20_204429) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -84,9 +84,6 @@ ActiveRecord::Schema[7.0].define(version: 2024_02_19_095725) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "first_name"
-    t.string "last_name"
-    t.text "description"
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
