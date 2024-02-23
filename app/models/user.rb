7e28821b4df5f1f9daf4331736ad2b5845eb9f89
Fernanda Borges Ferreira <eng.fernandabf@gmail.com>
2024-02-23 05:04:47 +0000

    request as a owner

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   has_many :pets
   has_many :requests
+  has_many :pet_requests, through: :pets, source: :requests
   has_one  :profile
 
   # PET OWNER START
