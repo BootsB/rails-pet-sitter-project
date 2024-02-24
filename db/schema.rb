5d4bfe06ad28c16c6ef8b0dceb7e60d7cfec55c1
Cristian Manolache <mlcristian@outlook.com>
2024-02-24 13:13:24 +0000

    done

--- db/schema.rb
+++ db/schema.rb
@@ -83,9 +83,6 @@ ActiveRecord::Schema[7.0].define(version: 2024_02_22_073044) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "first_name"
-    t.string "last_name"
-    t.text "description"
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
