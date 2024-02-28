c19ceb32bb142a165ac2fb4531dd0cfe8f30a6ae
CristianManolache <mlcristian@outlook.com>
2024-02-28 15:03:29 +0000

    fix

--- app/controllers/requests_controller.rb
+++ app/controllers/requests_controller.rb
@@ -1,4 +1,6 @@
 class RequestsController < ApplicationController
+  before_action :set_request, only: [:accept, :decline]
+
   def create
     @pet = Pet.find(params[:pet_id])
     @request = Request.new(user_id: current_user.id, pet_id: @pet.id)
@@ -7,6 +9,35 @@ class RequestsController < ApplicationController
       redirect_to @pet, notice: "Request sent!"
     else
       flash[:error] = "Failed to send request."
+      redirect_to @pet
     end
+  end
+
+  def accept
+    @request.accepted = true
+    if @request.save
+      redirect_to pets_path, notice: "Request accepted!"
+    else
+      flash[:error] = "Failed to accept request."
+      redirect_back(fallback_location: root_path)
+    end
+  end
+
+
+  def decline
+    @request.accepted = false
+    if @request.save
+      redirect_to pets_path, notice: "Request declined."
+    else
+      flash[:error] = "Failed to decline request."
+      redirect_back(fallback_location: root_path)
+    end
+  end
+
+
+  private
+
+  def set_request
+    @request = Request.find(params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,24 +1,23 @@
 Rails.application.routes.draw do
-  devise_for :users do
-  end
-  resources :profiles, only: [:new, :edit, :create, :update]
-  get '/profiles/:id', to: 'profiles#show', as: 'user_profile' #just for show profile
-  delete '/profiles/:id', to: 'profiles#destroy', as: 'delete_user_profile' #just for delete profile
-  delete '/profiles/:id/remove_image', to: 'profiles#remove_image', as: 'remove_profile_image' #just for remove image
+  devise_for :users
 
-  get '/dashboard', to: 'profiles#dashboard', as: 'request_dashboard' # request
+  resources :profiles, only: [:new, :edit, :create, :update]
+  get '/profiles/:id', to: 'profiles#show', as: 'user_profile'
+  delete '/profiles/:id', to: 'profiles#destroy', as: 'delete_user_profile'
+  delete '/profiles/:id/remove_image', to: 'profiles#remove_image', as: 'remove_profile_image'
+
+  get '/dashboard', to: 'profiles#dashboard', as: 'request_dashboard'
   get '/profiles/:id/dashboard', to: 'profiles#dashboard', as: 'dashboard'
 
   resources :pets do
-    resources :requests, only: [:create]
+    resources :requests, only: [:create] do
+      post :accept, on: :member
+      post :decline, on: :member
+    end
     collection do
       get :pet_listings
     end
   end
 
   root to: "pages#home"
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
 end
